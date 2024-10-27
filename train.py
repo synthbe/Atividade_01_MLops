78ac8d12254ad63a649cbd7170c1fea4e2e2ce66
synthbe <pedrom.varelac@gmail.com>
2024-10-27 23:23:10 +0000

    Update with new results

--- train.py
+++ train.py
@@ -11,14 +11,16 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
 
 if __name__ == "__main__":
-    drug_df = pd.read_csv('Data/drug200.csv')
-    drug_df = drug_df.sample(frac=1) # Randomizing the data
+    drug_df = pd.read_csv("Data/drug200.csv")
+    drug_df = drug_df.sample(frac=1)  # Randomizing the data
     drug_df.head()
 
-    X = drug_df.drop('Drug', axis=1).values
+    X = drug_df.drop("Drug", axis=1).values
     y = drug_df.Drug.values
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=32)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=32
+    )
 
     cat_col = [1, 2, 3]
     num_col = [0, 4]
@@ -26,7 +28,7 @@ if __name__ == "__main__":
     transform = ColumnTransformer(
         [
             ("encoder", OrdinalEncoder(), cat_col),
-            ("num_imputer", SimpleImputer(strategy='median'), num_col),
+            ("num_imputer", SimpleImputer(strategy="median"), num_col),
             ("num_scaler", StandardScaler(), num_col),
         ]
     )
@@ -42,11 +44,12 @@ if __name__ == "__main__":
 
     predictions = pipe.predict(X_test)
     accuracy = accuracy_score(y_test, predictions)
-    f1 = f1_score(y_test, predictions, average='macro')
+    f1 = f1_score(y_test, predictions, average="macro")
 
     with open("Results/metrics.txt", "w") as outfile:
-        outfile.write(f"\nAccuracy = {str(round(accuracy, 2) * 100)} %, F1 Score = {str(round(f1, 2))}.")
-
+        outfile.write(
+            f"\nAccuracy = {str(round(accuracy, 2) * 100)} %, F1 Score = {str(round(f1, 2))}."
+        )
 
     cm = confusion_matrix(y_test, predictions, labels=pipe.classes_)
     disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=pipe.classes_)
